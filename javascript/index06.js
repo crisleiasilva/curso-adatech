8ca8998b52cb0d6dc5f57a809f9fafc068f9b649
crisleiasilva <crisleisilva1905@gmail.com>
2024-01-10 14:11:05 -0300

    Criei index06js

--- javascript/index06.js
+++ javascript/index06.js
@@ -1,2 +1,16 @@
-console.log('Helo World!!!')
+//Utilize sempre o Camel case no JS
+var nomeDoCliente = "Crislei" //string
+var idade = 41  //number
+var altura = 1.70
+console.log(nomeDoCliente);
+console.log(idade);
+console.log(altura);
 
+let notaDoAluno = 8;
+const mediaDoAluno = 7.5;
+
+notaDoAluno = 9;
+
+
+console.log(notaDoAluno);
+console.log(mediaDoAluno);
